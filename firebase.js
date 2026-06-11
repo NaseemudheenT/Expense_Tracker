48d339b32332074ddb02da61347b436dcf33ba4b
Naseemudheen <t.naseemudheen@gmail.com>
2026-06-11 11:56:09 +0530

    Initialize Firebase app and export services

--- firebase.js
+++ firebase.js
@@ -1,1 +1,57 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js';
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile
+} from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js';
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  orderBy,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  serverTimestamp,
+  writeBatch
+} from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD6eUkcF1DR5Tj_oMNmJGHDm06XzKLGBg4',
+  authDomain: 'expense-tracker-5c250.firebaseapp.com',
+  projectId: 'expense-tracker-5c250',
+  storageBucket: 'expense-tracker-5c250.firebasestorage.app',
+  messagingSenderId: '770528221263',
+  appId: '1:770528221263:web:d85908f947ff233a07f23a',
+  measurementId: 'G-NGR7KC6ESL'
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export {
+  auth,
+  db,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  collection,
+  query,
+  where,
+  orderBy,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  serverTimestamp,
+  writeBatch
+};
 
