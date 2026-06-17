ad211b2f4f8d1d4c91a045a791198868834b5920
Naseemudheen <t.naseemudheen@gmail.com>
2026-06-17 11:02:29 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,26 +3,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import {
   getAuth,
   setPersistence,
-  browserLocalPersistence,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-  updateProfile,
+  browserLocalPersistence
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  deleteDoc,
-  doc,
-  getDocs,
-  query,
-  where,
-  orderBy,
-  writeBatch,
-  serverTimestamp,
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD6eUkcF1DR5Tj_oMNmJGHDm06XzKLGBg4",
@@ -31,34 +14,16 @@ const firebaseConfig = {
   storageBucket: "expense-tracker-5c250.firebasestorage.app",
   messagingSenderId: "770528221263",
   appId: "1:770528221263:web:d85908f947ff233a07f23a",
-  measurementId: "G-NGR7KC6ESL",
+  measurementId: "G-NGR7KC6ESL"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Keep users logged in after refresh
-setPersistence(auth, browserLocalPersistence).catch((e) =>
-  console.error("Persistence error:", e)
-);
+// Keep user logged in after page refresh
+setPersistence(auth, browserLocalPersistence).catch((err) => {
+  console.error("Auth persistence error:", err);
+});
 
-export {
-  auth,
-  db,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-  updateProfile,
-  collection,
-  addDoc,
-  deleteDoc,
-  doc,
-  getDocs,
-  query,
-  where,
-  orderBy,
-  writeBatch,
-  serverTimestamp,
-};
+export { auth, db };
